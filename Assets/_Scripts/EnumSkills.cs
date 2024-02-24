5df10b3ffe8ea95aac226f4c8dd76169f5b24190
Utku Karakuyu <utku20600@gmail.com>
2024-02-25 00:53:59 +0300

    .

---
+++ Assets/_Scripts/EnumSkills.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumSkills : MonoBehaviour
+{
+    public Skills skil;
+
+
+}
+public enum Skills
+{
+    skill1,
+    skill2,
+    skill3
+
+}
