41d620979d0e2dab344a82f9e11526ac7f51e280
Utku Karakuyu <utku20600@gmail.com>
2024-02-25 03:42:00 +0300

    .

--- Assets/_Scripts/Bird.cs
+++ Assets/_Scripts/Bird.cs
@@ -3,30 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour, IInteractable
+public class Bird : MonoBehaviour
 {
-    public Transform _targetPos;
-    private Animator _animator;
 
-    private void Start()
-    {
-        _animator = GetComponent<Animator>();
-    }
-    public void Interact()
-    {
-        FlyBird();
 
-    }
 
-    public void FlyBird()
-    {
-        gameObject.transform.DOMove(_targetPos.position, 9f).OnComplete(() =>
-        {
-            _animator.SetBool("flying", false);
-        });
-        _animator.SetBool("flying", true);
 
-        AudioSourceManager.Instance._sounds[6].Play();
-
-    }
 }
