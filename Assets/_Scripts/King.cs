9185a7a8c5e64dcaa68437e902984a1828481340
alper-dipci <dipcialper35@gmail.com>
2024-02-25 06:49:21 +0300

    .

--- Assets/_Scripts/King.cs
+++ Assets/_Scripts/King.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class King : Enemy
 {
     [SerializeField] List<GameObject> SkillVfx = new List<GameObject>();
+    [SerializeField] List<GameObject> skillIndicator = new List<GameObject>();
+    [SerializeField]private float indicatorTime;
+    [SerializeField]private float skillLifeTime;
+
+    Vector3 skillPos;
     public override void hit()
     {
+        // EGER KACIRIRSA UPDATE ICINDEN AL POZÝSYONU
+        skillPos = playerGameobject.transform.position;
+
         int skillNumber = Random.Range(1, SkillVfx.Count);
         isHitting = true;
         canHit = false;
@@ -22,11 +30,38 @@ public class King : Enemy
         switch (skillNumber)
         {
             case 1:
+                StartCoroutine(skill1());
                 break;
             case 2:
+                StartCoroutine(skill2());
                 break;
             case 3:
+                StartCoroutine(skill3());
                 break;
         }
+    }
+    private IEnumerator skill1()
+    {
+        GameObject indicator = Instantiate(skillIndicator[0],skillPos,Quaternion.identity);
+        yield return new WaitForSeconds(indicatorTime);
+        Destroy(indicator);
+        GameObject skill = Instantiate(skillIndicator[0], skillPos, Quaternion.identity);
+        Destroy(skill, skillLifeTime);
+    }
+    private IEnumerator skill2()
+    {
+        GameObject indicator = Instantiate(skillIndicator[1], skillPos, Quaternion.identity);
+        yield return new WaitForSeconds(indicatorTime);
+        Destroy(indicator);
+        GameObject skill = Instantiate(skillIndicator[1], skillPos, Quaternion.identity);
+        Destroy(skill, skillLifeTime);
+    }
+    private IEnumerator skill3()
+    {
+        GameObject indicator = Instantiate(skillIndicator[2], skillPos, Quaternion.identity);
+        yield return new WaitForSeconds(indicatorTime);
+        Destroy(indicator);
+        GameObject skill = Instantiate(skillIndicator[2], skillPos, Quaternion.identity);
+        Destroy(skill, skillLifeTime);
     }
 }
